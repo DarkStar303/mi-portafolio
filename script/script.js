7099be187c3a9787bf36db701d28b242536cfc2b
DarkStar303 <alejandroroyales2@gmail.com>
2025-11-28 23:08:55 -0400

    adicione animacion y bienvenida

--- script/script.js
+++ script/script.js
@@ -1,2 +1,6 @@
 console.log("bienvenido");
+//alert("gracias por visitar mi pagina web");
+document.querySelector('h1').addEventListener('click',()=>{
+
 alert("gracias por visitar mi pagina web");
+});
